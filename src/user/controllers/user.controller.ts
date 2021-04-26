eef5ac51150153c421a0d0cfc7d650f0bce5eb4e
José Cleiton Correia <jose.correia@cubos.io>
2021-04-26 08:51:46 -0300

    feat: test jwt guard

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Post, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../../auth/guards/jwt.guard';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UserService } from '../user.service';
 
-@Controller("user")
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
+  }
+
+  @Get(':enrollment')
+  @UseGuards(JwtGuard)
+  getUser(@Param('enrollment') enrollment: string) {
+    return this.userService.findUser({ enrollment });
   }
 
   // @Put()
