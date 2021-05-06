a19eb96eb880558a41154e8e0338b524119c6837
José Cleiton Correia <jcleitonbc@gmail.com>
2021-05-05 21:33:53 -0400

    feat: exam controller

--- src/exam/exam.controller.ts
+++ src/exam/exam.controller.ts
@@ -1,34 +1,54 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ExamService } from './exam.service';
 import { CreateExamDto } from './dto/create-exam.dto';
 import { UpdateExamDto } from './dto/update-exam.dto';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { ApiBearerAuth, ApiOkResponse } from '@nestjs/swagger';
+import { Exam } from './entities/exam.entity';
 
 @Controller('exam')
+@UseGuards(JwtGuard)
+@ApiBearerAuth()
 export class ExamController {
   constructor(private readonly examService: ExamService) {}
 
   @Post()
-  create(@Body() createExamDto: CreateExamDto) {
-    return this.examService.create(createExamDto);
+  @ApiOkResponse({ type: Exam })
+  async create(@Body() createExamDto: CreateExamDto): Promise<Exam> {
+    return await this.examService.create(createExamDto);
   }
 
   @Get()
-  findAll() {
-    return this.examService.findAll();
+  @ApiOkResponse({ type: [Exam] })
+  async findAll(): Promise<Exam[]> {
+    return await this.examService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.examService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<Exam> {
+    return await this.examService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateExamDto: UpdateExamDto) {
-    return this.examService.update(+id, updateExamDto);
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateExamDto: UpdateExamDto,
+  ): Promise<Exam> {
+    return await this.examService.update(id, updateExamDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.examService.remove(+id);
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return await this.examService.remove(id);
   }
 }
