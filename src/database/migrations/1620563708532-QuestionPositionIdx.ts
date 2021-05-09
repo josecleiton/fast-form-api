647516d481fe3a4723e2ce1e6c5449466ff2b8b5
José Cleiton Correia <jcleitonbc@gmail.com>
2021-05-09 08:54:07 -0400

    chore: db migration

--- src/database/migrations/1620563708532-QuestionPositionIdx.ts
+++ src/database/migrations/1620563708532-QuestionPositionIdx.ts
@@ -4,7 +4,6 @@ export class QuestionPositionIdx1620563708532 implements MigrationInterface {
     name = 'QuestionPositionIdx1620563708532'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("DELETE FROM `question`");
         await queryRunner.query("ALTER TABLE `question` DROP FOREIGN KEY `FK_3b2789ae1c494ff1bf8dd4d4607`");
         await queryRunner.query("ALTER TABLE `question` CHANGE `group_id` `group_id` int NOT NULL");
         await queryRunner.query("CREATE INDEX `IX_question_position` ON `question` (`position`)");
