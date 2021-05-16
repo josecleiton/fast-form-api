37aab95198100dd9fda80de7433e7e8fee8fabd0
coltran3 <luca.santos>
2021-05-15 20:11:37 -0400

    fix: add user type in createUser

--- src/user/helpers/create-user.helper.ts
+++ src/user/helpers/create-user.helper.ts
@@ -5,6 +5,7 @@ export function createUser(createUserDto: CreateUserDto): User {
   const user = new User();
   user.cpf = createUserDto.cpf;
   user.enrollment = createUserDto.enrollment;
+  user.type = createUserDto.type;
   user.role = createUserDto.role;
   return user;
 }
