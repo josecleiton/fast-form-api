28a9b5998d053939e3633d110ae7f43151a1fe80
José Cleiton Correia <jcleitonbc@gmail.com>
2021-05-29 19:58:43 -0400

    chore: eslint no used vars rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,11 +17,13 @@ module.exports = {
     jest: true,
   },
   rules: {
+    'no-unused-vars': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
     'no-await-in-loop': 'error',
   },
 };
