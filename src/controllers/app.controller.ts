02c671a8cd39336223957d99d5f0121bc72c0aa3
José Cleiton Correia <jcleitonbc@gmail.com>
2021-05-30 19:26:19 -0400

    chore: rename ping to health

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get('ping')
+  @Get('health')
   ping(): string {
-    return 'Pong';
+    return 'Healthy';
   }
 }
