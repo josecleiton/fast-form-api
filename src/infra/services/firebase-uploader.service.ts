973e2df40633b052b974c46ef3f65113406589d5
José Cleiton Correia <jcleitonbc@gmail.com>
2021-06-28 08:37:05 -0400

    fix: remove trailing slash

--- src/infra/services/firebase-uploader.service.ts
+++ src/infra/services/firebase-uploader.service.ts
@@ -17,7 +17,7 @@ export class FirebaseUploader implements UploaderService {
   }
 
   async upload(fileDto: FileSendDto, path = ''): Promise<string> {
-    return this.storageService.upload(this.getName(fileDto, path), {
+    return this.storageService.upload(this.getName(fileDto, path).replace(/^\//u, ''), {
       data: fileDto.buffer,
       contentType: fileDto.mimetype,
       publicFile: true,
