23942a687a07ec8380d3f903c0a0e468f7bab4b4
Cleber Del Rei <cleberdelrei@gmail.com>
2021-06-28 20:46:13 -0300

    refactor(exportation): query

--- src/exportation/exportation.repository.ts
+++ src/exportation/exportation.repository.ts
@@ -14,42 +14,31 @@ export class ExportationRepository {
 
     const csvLines: CsvLineRow[] = await queryRunner.query(
       `
-        SELECT DISTINCT
-            a.score AS 'score',
-            qg.id AS 'group_id',
-            qg.title AS 'group_title',
-            qg.class AS 'is_class',
-            qg.personal AS 'is_personal',
-            q.id AS 'question_id',
-            q.statement AS 'question_statement',
-            u.enrollment AS 'user_enrollment',
-            u.type AS 'user_type',
-            ea.id AS 'exam_agreement_id',
-            ea.anonymous AS 'anonymous',
-            ea.observation AS 'observation',
-            ea.uuid AS 'unique_code',
-            s.title AS 'subject_title'
-        FROM
-            exam ex
-        INNER JOIN exam_agreement ea ON
-            ea.exam_id = ex.id
-        INNER JOIN user u ON
-            u.id = ea.user_id
-        INNER JOIN answer a ON
-            a.exam_agreement_id = ea.id
-        LEFT JOIN grade ON
-            grade.id = a.grade_id
-        LEFT JOIN subject s ON
-            s.id = grade.subject_id
-        INNER JOIN question_group qg ON
-            qg.exam_id = ex.id
-        INNER JOIN question q ON
-            q.group_id = qg.id
-        WHERE
-            ex.id = ? AND
-            ex.deleted_at IS NULL
-        ORDER BY
-            ea.id ASC, qg.id ASC, q.id ASC
+      SELECT DISTINCT 
+        ea.exam_id AS 'exam_id',
+        ea.id AS 'exam_agreement_id',
+        ea.anonymous AS 'anonymous',
+        ea.observation AS 'observation',
+        ea.uuid AS 'unique_code',
+        qg.title AS 'group_title',
+        qg.class AS 'is_class',
+        qg.personal AS 'is_personal',
+        qg.id AS 'group_id',
+        q.id AS 'question_id',
+        q.statement AS 'question_statement',
+        a.id AS 'answer_id',
+        a.score AS 'score',
+        u.enrollment AS 'user_enrollment',
+        u.type AS 'user_type'
+      FROM exam_agreement ea
+        INNER JOIN USER u ON u.id = ea.user_id
+        INNER JOIN answer a ON a.exam_agreement_id = ea.id
+        JOIN grade g ON g.id = a.grade_id
+        INNER JOIN question q ON q.id = a.question_id
+        INNER JOIN question_group qg ON qg.id = q.group_id
+      WHERE ea.exam_id = ?
+        AND ea.deleted_at IS NULL
+      ORDER BY ea.id ASC, qg.id ASC, q.id ASC
       `,
       [examId],
     );
