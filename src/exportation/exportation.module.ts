11273eefe809da35c12c4889cbee1a967534fc20
Cleber Del Rei <cleberdelrei@gmail.com>
2021-06-27 21:12:56 -0300

    removing unused imports

--- src/exportation/exportation.module.ts
+++ src/exportation/exportation.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ExamRepository } from 'src/exam/repositories/exam.repository';
 import { ExportationController } from './exportation.controller';
 import { ExportationRepository } from './exportation.repository';
 import { ExportationService } from './exportation.service';
